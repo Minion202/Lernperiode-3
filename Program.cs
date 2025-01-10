df34c22042c1ea175677a96e58bebbea74a8b0d3
Minion202 <M.Urgancio.inf24@stud.bbbaden.ch>
2025-01-10 11:07:06 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        char[,] board = {
+            { '1', '2', '3' },
+            { '4', '5', '6' },
+            { '7', '8', '9' }
+        };
+
+        DrawBoard(board);
+    }
+
+    static void DrawBoard(char[,] board)
+    {
+        Console.Clear();
+        Console.WriteLine(" Tic Tac Toe ");
+        Console.WriteLine("-------------");
+
+        for (int i = 0; i < 3; i++)
+        {
+            Console.Write("| ");
+            for (int j = 0; j < 3; j++)
+            {
+                Console.Write(board[i, j] + " | ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("-------------");
+        }
+    }
+}
