022b3e8b42a7cd0656c2f62e0b200cec25413717
Minion202 <M.Urgancio.inf24@stud.bbbaden.ch>
2025-01-24 12:26:55 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,69 +4,114 @@ class Program
 {
     static void Main()
     {
-        char[,] board =
-        {
-            { ' ', ' ', ' ' },
-            { ' ', ' ', ' ' },
-            { ' ', ' ', ' ' }
-        };
+        char[,] board;
+        int scoreX = 0, scoreO = 0;
 
-        int playerX = 0, playerY = 0;
-        int playerX2 = 2, playerY2 = 2;
-        int cursorX = playerX, cursorY = playerY;
-        bool isPlayerXTurn = true;
         while (true)
         {
-            DrawBoard(board, cursorX, cursorY);
-
-            Console.WriteLine(isPlayerXTurn
-                ? "Player X, use arrow keys to move and press Enter to confirm."
-                : "Player O, use WASD keys to move and press Enter to confirm.");
-
-            ConsoleKey key = Console.ReadKey(true).Key;
-
-            if (isPlayerXTurn)
+            board = new char[3, 3]
             {
-                if (IsArrowKey(key))
+                { ' ', ' ', ' ' },
+                { ' ', ' ', ' ' },
+                { ' ', ' ', ' ' }
+            };
+
+            int playerX = 0, playerY = 0;
+            int playerX2 = 2, playerY2 = 2;
+            int cursorX = playerX, cursorY = playerY;
+            bool isPlayerXTurn = true;
+            bool gameOver = false;
+
+            while (!gameOver)
+            {
+                DrawBoard(board, cursorX, cursorY, scoreX, scoreO);
+
+                Console.WriteLine(isPlayerXTurn
+                    ? "Player X, use arrow keys to move and press Enter to confirm."
+                    : "Player O, use WASD keys to move and press Enter to confirm.");
+
+                ConsoleKey key = Console.ReadKey(true).Key;
+
+                if (isPlayerXTurn)
                 {
-                    MoveCursor(key, ref cursorX, ref cursorY);
-                }
-                else if (key == ConsoleKey.Enter)
-                {
-                    if (board[cursorY, cursorX] == ' ')
+                    if (IsArrowKey(key))
                     {
-                        board[cursorY, cursorX] = 'X';
-                        isPlayerXTurn = false; 
-                        cursorX = playerX2;
-                        cursorY = playerY2;
+                        MoveCursor(key, ref cursorX, ref cursorY);
                     }
-                    else
+                    else if (key == ConsoleKey.Enter)
                     {
-                        Console.WriteLine("Space is already occupied! Choose another.");
+                        if (board[cursorY, cursorX] == ' ')
+                        {
+                            board[cursorY, cursorX] = 'X';
+
+                            if (CheckWin(board, 'X'))
+                            {
+                                scoreX++;
+                                Console.Clear();
+                                Console.WriteLine("Player X wins!");
+                                gameOver = true;
+                            }
+                            else if (CheckDraw(board))
+                            {
+                                Console.Clear();
+                                Console.WriteLine("It's a draw!");
+                                gameOver = true;
+                            }
+                            else
+                            {
+                                isPlayerXTurn = false;
+                                cursorX = playerX2;
+                                cursorY = playerY2;
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Space is already chosen! Choose another.");
+                        }
+                    }
+                }
+                else
+                {
+                    if (IsWASDKey(key))
+                    {
+                        MoveCursor(key, ref cursorX, ref cursorY);
+                    }
+                    else if (key == ConsoleKey.Enter)
+                    {
+                        if (board[cursorY, cursorX] == ' ')
+                        {
+                            board[cursorY, cursorX] = 'O';
+
+                            if (CheckWin(board, 'O'))
+                            {
+                                scoreO++;
+                                Console.Clear();
+                                Console.WriteLine("Player O wins!");
+                                gameOver = true;
+                            }
+                            else if (CheckDraw(board))
+                            {
+                                Console.Clear();
+                                Console.WriteLine("It's a draw!");
+                                gameOver = true;
+                            }
+                            else
+                            {
+                                isPlayerXTurn = true;
+                                cursorX = playerX;
+                                cursorY = playerY;
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Space is already chosen! Choose another.");
+                        }
                     }
                 }
             }
-            else
-            {
-                if (IsWASDKey(key))
-                {
-                    MoveCursor(key, ref cursorX, ref cursorY);
-                }
-                else if (key == ConsoleKey.Enter)
-                {
-                    if (board[cursorY, cursorX] == ' ')
-                    {
-                        board[cursorY, cursorX] = 'O';
-                        isPlayerXTurn = true;
-                        cursorX = playerX;
-                        cursorY = playerY;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Space is already occupied! Choose another.");
-                    }
-                }
-            }
+
+            Console.WriteLine("Press any key to start a new game...");
+            Console.ReadKey();
         }
     }
 
@@ -105,10 +150,11 @@ class Program
                key == ConsoleKey.S || key == ConsoleKey.D;
     }
 
-    static void DrawBoard(char[,] board, int cursorX, int cursorY)
+    static void DrawBoard(char[,] board, int cursorX, int cursorY, int scoreX, int scoreO)
     {
         Console.Clear();
         Console.WriteLine(" Tic Tac Toe ");
+        Console.WriteLine("Player X: {0} | Player O: {1}", scoreX, scoreO);
         Console.WriteLine("----------------");
 
         for (int i = 0; i < 3; i++)
@@ -132,5 +178,29 @@ class Program
             Console.WriteLine();
             Console.WriteLine("----------------");
         }
+    }
+
+    static bool CheckWin(char[,] board, char player)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            if (board[i, 0] == player && board[i, 1] == player && board[i, 2] == player) return true;
+            if (board[0, i] == player && board[1, i] == player && board[2, i] == player) return true;
+        }
+
+        if (board[0, 0] == player && board[1, 1] == player && board[2, 2] == player) return true;
+        if (board[0, 2] == player && board[1, 1] == player && board[2, 0] == player) return true;
+
+        return false;
+    }
+
+    static bool CheckDraw(char[,] board)
+    {
+        foreach (char cell in board)
+        {
+            if (cell == ' ') return false;
+        }
+
+        return true;
     }
 }
